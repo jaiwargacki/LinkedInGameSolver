94c3925e1de5cea886b42b4a3fb40cb28764a4ac
Jai Wargacki <jpw5681@rit.edu>
2025-05-10 11:06:01 -0400

    Clean up code for tango solver

--- src/tango_solver.py
+++ src/tango_solver.py
@@ -35,16 +35,16 @@ class Rule:
         return True
 
 class Configuration:
-    def __init__(self, squares=None, rules=None, next_row=0, next_col=0):
+    def __init__(self, squares=None, rules=None, prev_row=0, prev_col=0):
         self.squares = squares if squares else [[None for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
         self.rules = rules if rules else []
-        self.next_row = next_row
-        self.next_col = next_col
+        self.prev_row = prev_row
+        self.prev_col = prev_col
 
     def make_move(self, position, value):
         if value not in (MOON, SUN):
             raise ValueError("Invalid value. Use 'M' for Moon or 'S' for Sun.")
-        if self.squares[position.row][position.col] is None:
+        elif self.squares[position.row][position.col] is None:
             self.squares[position.row][position.col] = value
         else:
             print(self)
@@ -56,57 +56,22 @@ class Configuration:
             raise ValueError("Invalid rule type. Use 'X' for Cross or '=' for Equal.")
         self.rules.append(rule)
 
+    def _validate_window(self, window, max_size=3):
+        total_moons = window.count(MOON)
+        total_suns = window.count(SUN)
+        return not (total_moons > max_size or total_suns > max_size)
+
     def validate_row_window(self, row, col):
-        total_moons = 0
-        total_suns = 0
-        for i in range(col, min(col + 3, GRID_SIZE)):
-            if self.squares[row][i] == MOON:
-                total_moons += 1
-            elif self.squares[row][i] == SUN:
-                total_suns += 1
-        if total_moons > 2 or total_suns > 2:
-            return False
-        else:
-            return True
+        return self._validate_window(self.squares[row][col:min(col + 3, GRID_SIZE)], 2)
 
     def validate_col_window(self, row, col):
-        total_moons = 0
-        total_suns = 0
-        for i in range(row, min(row + 3, GRID_SIZE)):
-            if self.squares[i][col] == MOON:
-                total_moons += 1
-            elif self.squares[i][col] == SUN:
-                total_suns += 1
-        if total_moons > 2 or total_suns > 2:
-            return False
-        else:
-            return True
+        return self._validate_window([self.squares[i][col] for i in range(row, min(row + 3, GRID_SIZE))], 2)
 
     def validate_row(self, row):
-        total_moons = 0
-        total_suns = 0
-        for col in range(GRID_SIZE):
-            if self.squares[row][col] == MOON:
-                total_moons += 1
-            elif self.squares[row][col] == SUN:
-                total_suns += 1
-        if total_moons > 3 or total_suns > 3:
-            return False
-        else:
-            return True
+        return self._validate_window(self.squares[row], 3)
 
     def validate_col(self, col):
-        total_moons = 0
-        total_suns = 0
-        for row in range(GRID_SIZE):
-            if self.squares[row][col] == MOON:
-                total_moons += 1
-            elif self.squares[row][col] == SUN:
-                total_suns += 1
-        if total_moons > 3 or total_suns > 3:
-            return False
-        else:
-            return True
+        return self._validate_window([self.squares[i][col] for i in range(GRID_SIZE)], 3)
 
     def validate_rule(self, rule):
         return rule.validate(self.squares[rule.position1.row][rule.position1.col], self.squares[rule.position2.row][rule.position2.col])
@@ -114,15 +79,18 @@ class Configuration:
     def get_next_row_col(self, row, col):
         next_col = col + 1 if col < GRID_SIZE - 1 else 0
         next_row = row if next_col > 0 else row + 1
-
         if next_row >= GRID_SIZE:
             return GRID_SIZE, GRID_SIZE
-        if self.squares[next_row][next_col] is None:
+        elif self.squares[next_row][next_col] is None:
             return next_row, next_col
         else:
             return self.get_next_row_col(next_row, next_col)
 
-        
+    def new_configuration(self, next_row, next_col, symbol):
+        next_moon = Configuration(copy.deepcopy(self.squares), self.rules, next_row, next_col)
+        next_moon.make_move(Position(next_row, next_col), MOON)
+        if next_moon.is_valid():
+            result.append(next_moon)
 
     def __str__(self):
         result = ""
@@ -155,19 +123,15 @@ class Configuration:
             return []
         result = []
 
-        next_row, next_col = self.get_next_row_col(self.next_row, self.next_col)
+        next_row, next_col = self.get_next_row_col(self.prev_row, self.prev_col)
         if next_row >= GRID_SIZE:
             return []
 
-        next_moon = Configuration(copy.deepcopy(self.squares), self.rules, next_row, next_col)
-        next_moon.make_move(Position(next_row, next_col), MOON)
-        if next_moon.is_valid():
-            result.append(next_moon)
-
-        next_sun = Configuration(copy.deepcopy(self.squares), self.rules, next_row, next_col)
-        next_sun.make_move(Position(next_row, next_col), SUN)
-        if next_sun.is_valid():
-            result.append(next_sun)
+        for symbol in (MOON, SUN):
+            next_config = Configuration(copy.deepcopy(self.squares), self.rules, next_row, next_col)
+            next_config.make_move(Position(next_row, next_col), symbol)
+            if next_config.is_valid():
+                result.append(next_config)
 
         return result
 
